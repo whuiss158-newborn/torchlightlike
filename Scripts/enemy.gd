ee71828d44eebf9968f958d727c9a50015eba9fc
whuiss158-mac <whuiss158@gmail.com>
2025-12-05 21:35:41 +0800

    feat: update

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -2,9 +2,14 @@ extends CharacterBody2D
 
 @export var enemy_move_speed = 20
 
+@onready var sp2d = $Sprite2D
 @onready var player = get_tree().get_first_node_in_group("player")
 
 func _physics_process(delta: float) -> void:
 	var direction = global_position.direction_to(player.global_position)
 	velocity = enemy_move_speed * direction
+	if velocity.x > 0.1:
+		sp2d.flip_h = true
+	elif velocity.x < -0.1:
+		sp2d.flip_h = false
 	move_and_slide()
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -3,10 +3,16 @@ extends CharacterBody2D
 
 const SPEED = 40
 
+@onready var sprite = $Sprite2D
+
 func movement():
 	var x_mov = Input.get_action_strength("right") - Input.get_action_strength("left")
 	var y_mov = Input.get_action_strength("down") - Input.get_action_strength("up")
 	var mov = Vector2(x_mov, y_mov)
+	if mov.x > 0:
+		sprite.flip_h = true
+	elif mov.x < 0:
+		sprite.flip_h = false
 	velocity = mov.normalized() * SPEED
 	print(velocity)
 	move_and_slide()
