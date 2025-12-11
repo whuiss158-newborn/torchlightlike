3052e776551088057ddfdb860a0b5563e3130734
whuiss158-mac <whuiss158@gmail.com>
2025-12-11 21:40:43 +0800

    update

--- Scripts/experience.gd
+++ Scripts/experience.gd
@@ -1,13 +1,13 @@
 extends Area2D
 
-@export var exp_amount = 1
+@export var exp_amount = 5
 @onready var sprite_2d: Sprite2D = $Sprite2D
 @onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
 @onready var audio_stream_player_2d: AudioStreamPlayer2D = $AudioStreamPlayer2D
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 var target = null
-var speed = 0
+var speed = -1
 
 var spr_green = preload("res://Assets/Textures/Items/Gems/Gem_green.png")
 var spr_blue = preload("res://Assets/Textures/Items/Gems/Gem_blue.png")
@@ -24,7 +24,7 @@ func _ready() -> void:
 func _physics_process(delta: float) -> void:
 	if not target == null:
 		global_position = global_position.move_toward(target.global_position, speed)
-		speed += 2 * delta
+		speed += 3 * delta
 		if global_position.distance_squared_to(target.global_position) < 10:
 			self.hide()
 			queue_free()
@@ -34,6 +34,4 @@ func collect(player: CharacterBody2D):
 	target = player
 	collision_shape_2d.call_deferred("set", "disabled", true)
 	animation_player.stop()
-	
-func _on_body_entered(body: Node2D) -> void:
-	print(body)
+	return exp_amount
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -3,11 +3,19 @@ extends CharacterBody2D
 
 const SPEED = 40
 var attack_speed = 1
+var attack_speed_growth = 1.3
+var player_level = 1
+var cur_exp = 0
+var upgrade_exp = 1
 
 @onready var sprite = $Sprite2D
 @onready var attack_cool_down_timer: Timer = $Attack/AttackCoolDownTimer
+@onready var experience_bar: TextureProgressBar = %ExperienceBar
+@onready var lbl_level: Label = %lbl_level
+@onready var level_up_panel: Panel = %LevelUpPanel
 
 var iceSpear = preload("res://Scenes/ice_spear.tscn")
+var upgradeCard = preload("res://Scenes/upgrade_card_item.tscn")
 var enemy_close: Array[CharacterBody2D] = []
 
 func _ready() -> void:
@@ -65,4 +73,42 @@ func _on_hurt_box_take_damage(damage: int) -> void:
 func _on_exp_detection_area_area_entered(area: Area2D) -> void:
 	if area.is_in_group("loot"):
 		if area.has_method("collect"):
-			area.call("collect", self)
+			var get_exp = area.call("collect", self)
+			handle_get_experience(get_exp)
+
+func calculate_upgrade_expericence():
+	if player_level < 5:
+		return player_level * 5
+	elif player_level < 10:
+		return player_level * 10
+	else	:
+		return 400 + player_level * 30
+	
+func handle_get_experience(exp: int):
+	cur_exp += exp
+	while cur_exp >= upgrade_exp:
+		player_level += 1
+		handle_level_up()
+		cur_exp -= upgrade_exp
+		upgrade_exp = calculate_upgrade_expericence()
+	update_experience_bar_status()
+
+func handle_level_up():
+	var card_container = level_up_panel.get_node("container_card")
+	level_up_panel.visible = true
+	var cardAmount = 3
+	var i = 1
+	while i <= cardAmount:
+		var card = upgradeCard.instantiate()
+		card_container.add_child(card)
+		i -= 1
+	attack_speed = attack_speed * attack_speed_growth
+	attack_cool_down_timer.wait_time = 1 / attack_speed
+	print(attack_cool_down_timer.wait_time)
+	get_tree().paused = true
+	
+func update_experience_bar_status():
+	experience_bar.value = cur_exp 
+	experience_bar.max_value = upgrade_exp
+	lbl_level.text = str("Level ", player_level)
+		
